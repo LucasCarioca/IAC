3d8c5035d8866a6087ccf67f5ac59b4f9f0e0fd7
Lucas Desouza <lucasfds2777@gmail.com>
2021-02-17 12:55:33 -0600

    fix formatting

--- main.tf
+++ main.tf
@@ -2,21 +2,21 @@ variable "tenant_id" {}
 variable "subscription_id" {}
 
 variable "resource_group_dev" {
-    default = "dev"
+  default = "dev"
 }
 
 variable "resource_group_prod" {
-    default = "prod"
+  default = "prod"
 }
 
 variable "resource_group_common" {
-    default = "common"
+  default = "common"
 }
 
 provider "azurerm" {
   version = ">= 2.0"
   features {}
-  tenant_id = var.tenant_id
+  tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
 }
 
@@ -28,17 +28,17 @@ data "azurerm_resource_group" "prod-rg" {
   name = var.resource_group_prod
 }
 
-data "azurerm_container_registry" "common-acr"{
-  name = "desouza"
+data "azurerm_container_registry" "common-acr" {
+  name                = "desouza"
   resource_group_name = var.resource_group_common
 }
 
 resource "azurerm_app_service_plan" "dev-serviceplan" {
-  name = "dev-appserviceplan"
-  location = data.azurerm_resource_group.dev-rg.location
+  name                = "dev-appserviceplan"
+  location            = data.azurerm_resource_group.dev-rg.location
   resource_group_name = data.azurerm_resource_group.dev-rg.name
-  kind = "Linux"
-  reserved = true
+  kind                = "Linux"
+  reserved            = true
 
   sku {
     tier = "Basic"
@@ -47,11 +47,11 @@ resource "azurerm_app_service_plan" "dev-serviceplan" {
 }
 
 resource "azurerm_app_service_plan" "prod-serviceplan" {
-  name = "prod-appserviceplan"
-  location = data.azurerm_resource_group.prod-rg.location
+  name                = "prod-appserviceplan"
+  location            = data.azurerm_resource_group.prod-rg.location
   resource_group_name = data.azurerm_resource_group.prod-rg.name
-  kind = "Linux"
-  reserved = true
+  kind                = "Linux"
+  reserved            = true
 
   sku {
     tier = "Basic"
