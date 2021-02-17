8903ee8d6ec90de846efe158ce39012095b81a91
Lucas Desouza <lucasfds2777@gmail.com>
2021-02-17 14:52:14 -0600

    fixing typo

--- apps.tf
+++ apps.tf
@@ -3,8 +3,8 @@
 module "example-html-app" {
   source                   = "./app"
   name                     = "example-html-app-setup"
-  app_service_plan_id_dev  = zurerm_app_service_plan.dev-serviceplan.id
-  app_service_plan_id_prod = zurerm_app_service_plan.prod-serviceplan.id
+  app_service_plan_id_dev  = azurerm_app_service_plan.dev-serviceplan.id
+  app_service_plan_id_prod = azurerm_app_service_plan.prod-serviceplan.id
   image                    = "htmlex"
   app_settings_dev         = {}
   app_settings_prod        = {}
