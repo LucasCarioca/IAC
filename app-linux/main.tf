37f66e382d3d30417a0fce177feb53f13c4c6f92
Lucas Desouza <lucasfds2777@gmail.com>
2021-03-05 23:34:51 -0600

    fix modules

---
+++ app-linux/main.tf
@@ -0,0 +1,35 @@
+resource "azurerm_app_service" "app-service-dev" {
+  name                = "${var.name}-dev"
+  location            = "centralus"
+  resource_group_name = "dev"
+  app_service_plan_id = var.app_service_plan_id_dev
+
+  site_config {
+    always_on        = true
+    linux_fx_version = "NODE|12-lts"
+  }
+
+  app_settings = merge(
+    var.app_settings_dev,
+    {
+    }
+  )
+}
+
+resource "azurerm_app_service" "app-service-prod" {
+  name                = var.name
+  location            = "centralus"
+  resource_group_name = "prod"
+  app_service_plan_id = var.app_service_plan_id_prod
+
+  site_config {
+    always_on        = true
+    linux_fx_version = "NODE|12-lts"
+  }
+
+  app_settings = merge(
+    var.app_settings_dev,
+    {
+    }
+  )
+}
